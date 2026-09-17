a0b614d31693f312a21fd24b8f22100c18a01dfc
AkspertLemon <abdullahfarhantcs@gmail.com>
2026-09-17 11:06:51 +0500

    Question 3 and 4

---
+++ September/Lab_4 Post/Question4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+  float length, width;
+  printf("Enter length: ");
+  scanf("%f", &length);
+  printf("Enter width: ");
+  scanf("%f", &width);
+ 
+  printf("Area of rectangle is %f", length * width); 
+}
---
+++ September/Lab_4 Post/Question5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int Num;
+    printf("\n");
+    printf("Enter Number: ");
+    scanf("%d",&Num);
+    printf("\n");
+    printf("Square of %d is %d",Num,Num*Num);
+    printf("\n");
+    printf("Cube of %d is %d",Num,Num*Num*Num);
+    return 0;
+}
