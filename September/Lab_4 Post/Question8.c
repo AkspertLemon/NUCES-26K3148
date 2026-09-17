9824da08b3df2d1a83f779106e665d274eb49c9b
AkspertLemon <abdullahfarhantcs@gmail.com>
2026-09-17 11:21:31 +0500

    Question 8

---
+++ September/Lab_4 Post/Question8.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int Marks;
+    printf("Enter Marks:\n");
+    scanf("%d",&Marks);
+    if (Marks>=50)
+    {
+        printf("Pass");
+    }
+    else{
+        printf("Fail");
+    }
+    
+}
